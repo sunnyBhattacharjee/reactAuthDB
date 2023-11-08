d639cc8fe15a59f03101050a29284c6eae3c1c90
sunnyBhattacharjee <sunny141087@gmail.com>
2023-11-08 20:58:12 +0530

    formatted code

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 const express = require("express");
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 const brcypt = require("bcryptjs");
 const app = express();
-const bodyParser = require('body-parser');
-const dbConnect = require("./db/dbconnect")
-const User = require("./db/userModel") 
+const bodyParser = require("body-parser");
+const dbConnect = require("./db/dbconnect");
+const User = require("./db/userModel");
 const auth = require("./auth");
 
 // body parser configuration
 app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (request, response, next) => {
   response.json({ message: "Hey! This is your server response!" });
   next();
 });
 
-dbConnect()
+dbConnect();
 
 // Curb Cores Error by adding a header here
 app.use((req, res, next) => {
@@ -32,68 +32,71 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post('/register',(request,response) => {
-  brcypt.hash(request.body.password,10).then((hashedPassword)=>{
-    const user = new User({
-      email: request.body.email,
-      password: hashedPassword,
-    });
+app.post("/register", (request, response) => {
+  brcypt
+    .hash(request.body.password, 10)
+    .then((hashedPassword) => {
+      const user = new User({
+        email: request.body.email,
+        password: hashedPassword,
+      });
 
-    user.save().then((result)=>{
-      response.status(201).send({
-        message:"user created Successfully",
-        result
-      })
-    })
-    .catch((error)=> {
-      response.status(500).send({
-        message:"Error in User Creation",
-        error
-      })
-    })
-  })
-  .catch((e)=> {
-    response.status(500).send({
-      message: "Password was not hashed successfully",
-      e,
-    });
-  })
-})
-
-
-app.post('/login',(request,response)=> {
-  User.findOne({email: request.body.email}).then((user) => {
-    brcypt.compare(request.body.password,user.password).then(res => {
-      if(!res){
-        return response.status(400).send({
-          message: "Passwords does not match",
+      user
+        .save()
+        .then((result) => {
+          response.status(201).send({
+            message: "user created Successfully",
+            result,
+          });
+        })
+        .catch((error) => {
+          response.status(500).send({
+            message: "Error in User Creation",
+            error,
+          });
         });
-      }
-
-      const token = jwt.sign(
-        {
-          userId : user._id,
-          userEmail:user.email
-        },
-        "RANDOM-TOKEN",
-        {expiresIn : "12h"}
-      )
-
-      response.status(200).send({
-        message: "Login Successful",
-        email: user.email,
-        token
-      })
-
     })
-  })
-  .catch((error)=> {
+    .catch((e) => {
+      response.status(500).send({
+        message: "Password was not hashed successfully",
+        e,
+      });
+    });
+});
+
+app.post("/login", (request, response) => {
+  User.findOne({ email: request.body.email })
+    .then((user) => {
+      brcypt.compare(request.body.password, user.password).then((res) => {
+        if (!res) {
+          return response.status(400).send({
+            message: "Passwords does not match",
+          });
+        }
+
+        const token = jwt.sign(
+          {
+            userId: user._id,
+            userEmail: user.email,
+          },
+          "RANDOM-TOKEN",
+          { expiresIn: "12h" }
+        );
+
+        response.status(200).send({
+          message: "Login Successful",
+          email: user.email,
+          token,
+        });
+      });
+    })
+    .catch((error) => {
       response.status(404).send({
-        message:"Email not found",
-        error
-      })
-   })
-})
+        message: "Email not found",
+        error,
+      });
+    });
+});
 
 // free endpoint
 app.get("/free-endpoint", (request, response) => {
--- auth.js
+++ auth.js
@@ -1,16 +1,15 @@
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 
-module.exports = async (request , response , next) => {
-    try{
-        const token = await request.headers.authorization.split(" ")[1];  
-        const decodeToken = await jwt.verify(token,"RANDOM-TOKEN");
-        const user = await decodeToken;
-        request.user = user;
-        next();
-    }catch(e){
-        response.status(401).json({
-            error: new Error("Invalid request!"),
-        })
-    }
-}
-
+module.exports = async (request, response, next) => {
+  try {
+    const token = await request.headers.authorization.split(" ")[1];
+    const decodeToken = await jwt.verify(token, "RANDOM-TOKEN");
+    const user = await decodeToken;
+    request.user = user;
+    next();
+  } catch (e) {
+    response.status(401).json({
+      error: new Error("Invalid request!"),
+    });
+  }
+};
